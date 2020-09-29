bcd07343dc2ec3f6bffb9457ffec459f71e6a25f
pyd_typer <pydtyper@gmail.com>
2020-09-29 16:29:46 -0400

    added skeleton functions

--- app.py
+++ app.py
@@ -1,1 +1,64 @@
-print("Testing!")
+import datetime
+import os
+
+from peewee import *
+
+db = SqliteDatabase("inventory.db")
+
+class Product(Model):
+
+    product_id = IntegerField(primary_key=True)
+    product_name = CharField(max_length=255)
+    product_quantity = IntegerField(default=0)
+    product_price = IntegerField(default=0.0)
+    date_updated = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+def initialize():
+    db.connect()
+    db.create_tables([Product], safe=True)
+
+
+def menu_loop():
+    """show the menu"""
+    # print if user doesn't type v, a, or b, then reloop
+    choice = None
+    while choice != "q":
+        clear()
+
+def view_inventory():
+    """view the inventory"""
+    pass
+
+
+def add_from_csv():
+    """add items from inventory.csv"""
+    pass
+
+
+def show_by_id():
+    """show item by id v"""
+    #print error if ID doesn't exist
+    pass
+
+def add_item():
+    """add item to inventory a"""
+    pass
+    #if dup found while trying to add, check which entry most recent and only save that data
+
+def backup_database():
+    """backup the database b"""
+    # the backup CSV file should only contain a single header row with all field titles
+    pass
+
+
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+if __name__ == "__main__":
+    initialize()    
+    menu_loop()
