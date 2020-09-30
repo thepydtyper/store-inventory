ae94d5a90b306bc20f1a65a9a3cd3e3243686feb
pyd_typer <pydtyper@gmail.com>
2020-09-30 10:34:19 -0400

    added menu_loop

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import datetime
 import os
+import sys
+import csv
+from collections import OrderedDict
 
 from peewee import *
 
@@ -21,13 +24,27 @@ def initialize():
     db.connect()
     db.create_tables([Product], safe=True)
 
+    with open("inventory.csv", "a") as file:
+        data = csv.reader(file, delimiter=",")
+    print(data)
+
 
 def menu_loop():
     """show the menu"""
     # print if user doesn't type v, a, or b, then reloop
     choice = None
     while choice != "q":
-        clear()
+        #clear()
+        print("Enter 'q' to quit.")
+        for key, value in menu.items():
+            print(f"{key}) {value.__doc__}")
+
+        choice = input("\nAction: ").lower().strip()
+        
+        if choice in menu:
+            #clear()
+            menu[choice]()
+
 
 def view_inventory():
     """view the inventory"""
@@ -40,23 +57,30 @@ def add_from_csv():
 
 
 def show_by_id():
-    """show item by id v"""
+    """show item by id"""
     #print error if ID doesn't exist
-    pass
+    print("showing id..")
 
 def add_item():
-    """add item to inventory a"""
-    pass
+    """add item to inventory"""
     #if dup found while trying to add, check which entry most recent and only save that data
+    print("adding item..")
 
 def backup_database():
-    """backup the database b"""
+    """backup the database"""
     # the backup CSV file should only contain a single header row with all field titles
-    pass
+    print("backinig up db..")
 
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
+
+
+menu = OrderedDict([
+    ('a', add_item),
+    ('v', show_by_id),
+    ('b', backup_database)
+])
 
 
 if __name__ == "__main__":
