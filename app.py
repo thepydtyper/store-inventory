95a7cf625234ab1ee962e4c55c8df2e9dc1e6afe
pyd_typer <pydtyper@gmail.com>
2020-09-30 16:06:04 -0400

    added import, cleaning, and backup of data

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 
 from peewee import *
 
+
 db = SqliteDatabase("inventory.db")
 
 class Product(Model):
@@ -23,53 +24,106 @@ class Product(Model):
 def initialize():
     db.connect()
     db.create_tables([Product], safe=True)
-
-    with open("inventory.csv", "a") as file:
-        data = csv.reader(file, delimiter=",")
-    print(data)
+    cleaned_data = clean_csv_data()
+    build_database(cleaned_data)
 
 
 def menu_loop():
     """show the menu"""
+
     # print if user doesn't type v, a, or b, then reloop
     choice = None
-    while choice != "q":
-        #clear()
-        print("Enter 'q' to quit.")
+    while choice not in ['q', 'a', 'v', 'b']:
+        print("\n--Inventory System--\n")
         for key, value in menu.items():
             print(f"{key}) {value.__doc__}")
 
-        choice = input("\nAction: ").lower().strip()
+        choice = input("\nAction: >> ").lower().strip()
         
         if choice in menu:
-            #clear()
+            clear()
             menu[choice]()
+        else:
+            print("ERROR: Please enter either: 'a', 'v', 'b', or 'q'\n")
 
 
-def view_inventory():
-    """view the inventory"""
-    pass
+def clean_csv_data():
+    """cleans fields from inventory.csv"""
+
+    with open("inventory.csv", newline="") as file:
+        data = csv.DictReader(file, delimiter=",")
+        rows = list(data)
+
+        for row in rows:
+            row["product_quantity"] = int(row["product_quantity"])
+            row["product_price"] = int(row["product_price"].replace("$", "").replace(".", ""))
+            row["date_updated"] = datetime.datetime.strptime(row["date_updated"], "%m/%d/%Y")
+    
+    return rows
 
 
-def add_from_csv():
-    """add items from inventory.csv"""
-    pass
+def build_database(data):
+    """adds cleaned data into the inventory.db file"""
+
+    for row in data:
+        try:
+            Product.create(
+                product_name = row["product_name"],
+                product_price = row["product_price"],
+                product_quantity = row["product_quantity"],
+                date_updated = row["date_updated"]
+            ).save()
+        except IntegrityError:
+            product = Product.get(product_name=row["product_name"])
+            product.product_name = row["product_name"]
+            product.product_quantity = row["product_quantity"]
+            product.product_price = row["product_price"]
+            product.date_updated = row["date_updated"]
+            product.save()
 
 
 def show_by_id():
     """show item by id"""
+
     #print error if ID doesn't exist
     print("showing id..")
 
+
 def add_item():
     """add item to inventory"""
+
     #if dup found while trying to add, check which entry most recent and only save that data
     print("adding item..")
 
+
 def backup_database():
     """backup the database"""
-    # the backup CSV file should only contain a single header row with all field titles
+
     print("backinig up db..")
+    with open("db_backup.csv", 'w') as backup:
+        headers = ["product_id", "product_name", "product_price", "product_quantity", "date_updated"]
+        dbwriter = csv.DictWriter(backup, fieldnames=headers)
+
+        dbwriter.writeheader()
+        
+        inventory = Product.select()
+        for item in inventory:
+            dbwriter.writerow({
+                "product_id": item.product_id,
+                "product_name": item.product_name,
+                "product_price": item.product_price,
+                "date_updated": item.date_updated
+            })
+
+    if not os.path.isfile("db_backup.csv"):
+        print("Error during backup..")
+
+
+
+def escape():
+    """exit the program"""
+
+    sys.exit()
 
 
 def clear():
@@ -79,7 +133,8 @@ def clear():
 menu = OrderedDict([
     ('a', add_item),
     ('v', show_by_id),
-    ('b', backup_database)
+    ('b', backup_database),
+    ('q', escape)
 ])
 
 
